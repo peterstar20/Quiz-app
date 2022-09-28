282f52dea5c68377042c2287d05a512e993858c4
peterstar20 <64819262+peterstar20@users.noreply.github.com>
2022-09-28 20:20:56 +0100

    Add files via upload

---
+++ Quiz app/js/questions.js
@@ -0,0 +1,62 @@
+//creating an array and passing the number, questions, options, and answers
+let questions  = [
+  {
+      numb: 1,
+      question: "What's the square root of 144?",
+      answer: "12",
+      options: [
+          "17",
+          "12",
+          "14",
+          "11"
+      ]
+  },
+
+  {
+      numb: 2,
+      question: "What's the full meaning of UN",
+      answer: "United Nation",
+      options: [
+          "Universe Nation",
+          "United Nates",
+          "Unicorn Naives",
+          "United Nation"
+      ]
+  },
+
+  {
+      numb: 3,
+      question: "A stich in time saves _______?",
+      answer: "nine",
+      options: [
+          "eleven",
+          "nine",
+          "seven",
+          "twelve"
+      ]
+  },
+
+  {
+      numb: 4,
+      question: "What does CSS stands for",
+      answer: "Cascading Style Sheet",
+      options: [
+          "Common Style Sheet",
+          "Colour Style Sheet",
+          "Computer Style Sheet",
+          "Cascading Style Sheet"
+      ]
+  },
+
+  {
+      numb: 5,
+      question: "Green in the traffic light means what?",
+      answer: "Go",
+      options: [
+          "Speed",
+          "Go",
+          "Remain",
+          "Stop"
+      ]
+  }
+];
